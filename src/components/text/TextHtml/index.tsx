7d7fef6bb06673ad827db64572eddfc581cf11ef
Zinoviev Artem <7binary@gmail.com>
2021-04-24 09:25:36 +0300

    Added open external Links @ TextHtml

--- src/components/text/TextHtml/index.tsx
+++ src/components/text/TextHtml/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import { Dimensions, View, ViewStyle, TouchableOpacity, Text, TextStyle } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import { Dimensions, View, ViewStyle, Linking } from 'react-native';
 import HTML from 'react-native-render-html';
 import { IGNORED_TAGS } from 'react-native-render-html/src/HTMLUtils';
-import uuid from 'uuidv4';
-
 import { FitImage } from '@components/FitImage';
+
+interface Props {
+  children: any;
+  containerStyle?: ViewStyle;
+}
 
 const includeTags =
   ['img', 'table', 'caption', 'col', 'colgroup', 'tbody', 'td', 'tfoot', 'th', 'thead', 'tr'];
@@ -41,55 +43,46 @@ const thStyle: ViewStyle = {
   alignItems: 'center',
 };
 
-const aStyle: TextStyle = {
-  marginBottom: -3,
-};
-
-interface Props {
-  children: any;
-  containerStyle?: ViewStyle;
-}
-
 export const TextHtml: React.FC<Props> = ({ children, containerStyle }) => {
-  const navigation = useNavigation();
-
   const renderers = {
-    table: (x: any, c: any) => <View key={`${c}${uuid()}`} style={tableColumnStyle}>{c}</View>,
-    col: (x: any, c: any) => <View key={`${c}${uuid()}`} style={tableColumnStyle}>{c}</View>,
-    colgroup: (x: any, c: any) => <View key={`${c}${uuid()}`} style={tableRowStyle}>{c}</View>,
-    tbody: (x: any, c: any) => <View key={`${c}${uuid()}`} style={tableColumnStyle}>{c}</View>,
-    tfoot: (x: any, c: any) => <View key={`${c}${uuid()}`} style={tableRowStyle}>{c}</View>,
-    th: (x: any, c: any) => <View key={`${c}${uuid()}`} style={thStyle}>{c}</View>,
-    thead: (x: any, c: any) => <View key={`${c}${uuid()}`} style={tableRowStyle}>{c}</View>,
-    caption: (x: any, c: any) => <View key={`${c}${uuid()}`} style={tableColumnStyle}>{c}</View>,
-    tr: (x: any, c: any) => <View key={`${c}${uuid()}`} style={tableRowStyle}>{c}</View>,
-    td: (x: any, c: any) => <View key={`${c}${uuid()}`} style={tdStyle}>{c}</View>,
+    table: (x: any, c: any) => <View key={`${c}`} style={tableColumnStyle}>{c}</View>,
+    col: (x: any, c: any) => <View key={`${c}`} style={tableColumnStyle}>{c}</View>,
+    colgroup: (x: any, c: any) => <View key={`${c}`} style={tableRowStyle}>{c}</View>,
+    tbody: (x: any, c: any) => <View key={`${c}`} style={tableColumnStyle}>{c}</View>,
+    tfoot: (x: any, c: any) => <View key={`${c}`} style={tableRowStyle}>{c}</View>,
+    th: (x: any, c: any) => <View key={`${c}`} style={thStyle}>{c}</View>,
+    thead: (x: any, c: any) => <View key={`${c}`} style={tableRowStyle}>{c}</View>,
+    caption: (x: any, c: any) => <View key={`${c}`} style={tableColumnStyle}>{c}</View>,
+    tr: (x: any, c: any) => <View key={`${c}`} style={tableRowStyle}>{c}</View>,
+    td: (x: any, c: any) => <View key={`${c}`} style={tdStyle}>{c}</View>,
     img: (x: any) => (
       <FitImage
         url={x.src}
-        key={`${x.src}${uuid()}`}
+        key={`${x.src}`}
         defaultWidth={Dimensions.get('screen').width}
         defaultHeight={600}
       />
     ),
-    a: (x: any, c: any) => (
-      <TouchableOpacity
-        onPress={() => navigation.navigate('Webview', { url: x.href })}
-        key={`${x.href}${uuid()}`}
-        style={aStyle}
-      >
-        <Text>{c}</Text>
-      </TouchableOpacity>
-    ),
   };
+
+  const maxWidth = Dimensions.get('window').width;
 
   return (
     <View style={containerStyle}>
       <HTML
         html={children}
-        imagesMaxWidth={Dimensions.get('window').width}
+        imagesMaxWidth={maxWidth}
+        staticContentMaxWidth={maxWidth}
+        onLinkPress={(evt, href) => Linking.openURL(href)}
         ignoredTags={tags}
         renderers={renderers}
+        alterChildren={node => {
+          if (node.name === 'iframe' || node.name === 'img') {
+            delete node.attribs.width;
+            delete node.attribs.height;
+          }
+          return node.children;
+        }}
       />
     </View>
   );
